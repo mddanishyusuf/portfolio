8b1ddb488847b674111e7d32bfc7fba09d9ab3e9
mohd danish <mddanishyusuf@gmail.com>
2019-11-29 04:57:56 +0800

    building portfolio with gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
+
+const IndexPage = () => (
+    <Layout>
+        <SEO title="Home" />
+        Hey, I'm Mohd Danish
+    </Layout>
+);
+
+export default IndexPage;
